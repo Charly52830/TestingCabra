a52e2c696ff0a0b192ad1cc230e569c33a1e66e2
Charly52830 <g.m.52830A@gmail.com>
2019-11-30 23:19:16 -0600

    Refactor home page view to use a template

---
+++ Cabra/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/vagrant/Django/Cabra/Cabra/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 
 from lists.views import home_page
 
-
+"""
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
@@ -16,6 +16,25 @@ class HomePageTest(TestCase):
         request = HttpRequest()  
         response = home_page(request)  
         html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))  
+        self.assertTrue(html.strip().startswith('<html>'))  
         self.assertIn('<title>To-Do lists</title>', html)  
-        self.assertTrue(html.endswith('</html>')) 
+        self.assertTrue(html.strip().endswith('</html>'))
+    
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')  
+
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.strip().startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')  
+        self.assertTemplateUsed(response, 'wrong.html')
+"""
+    
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
